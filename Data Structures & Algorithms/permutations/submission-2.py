fe43a8d324e8fbdcce31f37be58ae6bd45b39037
prishalala <71511521+prishalala@users.noreply.github.com>
2026-06-10 12:45:09 +0400

    Add: permutations - submission-2

---
+++ Data Structures & Algorithms/permutations/submission-2.py
@@ -0,0 +1,16 @@
+from typing import List
+
+class Solution:
+    def permute(self, nums: List[int]) -> List[List[int]]:
+        res = []
+
+        def dfs(path, remaining):
+            if not remaining:
+                res.append(path)
+                return
+
+            for i in range(len(remaining)):
+                dfs(path + [remaining[i]], remaining[:i] + remaining[i+1:])
+
+        dfs([], nums)
+        return res
